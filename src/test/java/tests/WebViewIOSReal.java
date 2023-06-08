ea54a68cc7beb4ef7f940b7c0d41b54843fb807f
Mehmet Usta <ustamehmet83@hotmail.com>
2023-06-08 20:10:40 +0300

    AppiumPractice

---
+++ src/test/java/tests/WebViewIOSReal.java
@@ -0,0 +1,33 @@
+package tests;
+
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.ios.IOSDriver;
+import org.openqa.selenium.By;
+import utils.Driver;
+
+import java.time.Duration;
+import java.util.Set;
+
+public class WebViewIOSReal {
+
+    public static void main(String[] args) throws Exception {
+        AppiumDriver driver = Driver.getDriver("iOS");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        driver.findElement(AppiumBy.accessibilityId("Or connect using a social account.")).click();
+        driver.findElement(AppiumBy.accessibilityId("Google")).click();
+//        Thread.sleep(5000);
+
+        Set<String> contextHandles = ((IOSDriver) driver).getContextHandles();
+        for (Object contextHandle : contextHandles) {
+            System.out.println(contextHandle.toString());
+        }
+        ((IOSDriver) driver).context(contextHandles.toArray()[1].toString());
+        System.out.println(driver.findElement(By.cssSelector("#headingText > span")).getText());
+        System.out.println(driver.findElement(By.xpath("//*[@id=\"headingText\"]/span")).getText());
+
+        ((IOSDriver) driver).context("NATIVE_APP");
+        driver.quit();
+    }
+}
--- src/test/java/utils/Driver.java
+++ src/test/java/utils/Driver.java
@@ -37,7 +37,7 @@ public class Driver {
                         + File.separator + "resources"
                         + File.separator + "ApiDemos-debug.apk";
                 caps.setCapability(MobileCapabilityType.APP, appUrl);
-               // caps.setCapability("chromedriverExecutable","\"C:\\Users\\mehme\\OneDrive\\Desktop\\Appium\\chromedriver.exe\"");
+                //caps.setCapability("chromedriverExecutable","\"C:\\Users\\mehme\\OneDrive\\Desktop\\Appium\\chromedriver.exe\"");
                 //caps.setCapability("unlockType","pattern");
                 //caps.setCapability("unlockKey","123456789");
                 System.out.println("*** Android App ***");
@@ -54,7 +54,6 @@ public class Driver {
                         + File.separator + "main"
                         + File.separator + "resources"
                         + File.separator + "UIKitCatalog-iphonesimulator.app";
-
                 caps.setCapability("simulatorStartupTimeout",180000);
                 caps.setCapability("bundleId","com.example.apple-samplecode.UICatalog");
                 System.out.println("***iOS Mobile Web-Safari ***");
@@ -68,18 +67,19 @@ public class Driver {
                         + File.separator + "main"
                         + File.separator + "resources"
                         + File.separator + "ApiDemos-debug.apk";
-               // caps.setCapability(MobileCapabilityType.APP, appUrl2);
+               //caps.setCapability(MobileCapabilityType.APP, appUrl2);
                 caps.setCapability("appPackage","com.google.android.apps.maps");
                 caps.setCapability("appActivity","com.google.android.maps.MapsActivity");
 
                 System.out.println("*** Android App ***");
                 return new AndroidDriver (url,caps);
             case "safari":
-                caps.setCapability("platformName","iOS");
-                caps.setCapability("browserName","Safari");
-                caps.setCapability(MobileCapabilityType.PLATFORM_VERSION,"12.4");
+                caps.setCapability(MobileCapabilityType.PLATFORM_NAME,"iOS");
                 caps.setCapability(MobileCapabilityType.AUTOMATION_NAME,"XCUITest");
-                caps.setCapability(MobileCapabilityType.DEVICE_NAME,"iPhone X");
+                caps.setCapability(MobileCapabilityType.BROWSER_NAME,"Safari");
+                caps.setCapability(MobileCapabilityType.PLATFORM_VERSION,"12.4");
+                caps.setCapability("simulatorStartupTimeout",180000);
+                //caps.setCapability(MobileCapabilityType.DEVICE_NAME,"iPhone X");
                 System.out.println("***iOS Mobile Web-Safari ***");
                 return new IOSDriver(new URL("http://0.0.0.0:4723/wd/hub"),caps);
             case "chrome":
@@ -87,22 +87,12 @@ public class Driver {
                 caps.setCapability(MobileCapabilityType.AUTOMATION_NAME,"uiautomator2");
                 caps.setCapability(MobileCapabilityType.UDID,"emulator-5554");
                 caps.setCapability(MobileCapabilityType.BROWSER_NAME,"chrome");
-                //androidCaps.setCapability(MobileCapabilityType.PLATFORM_VERSION,"11.0");
-                //androidCaps.setCapability("skipUnlock","true");
-                //androidCaps.setCapability("fullReset","true");
-                //androidCaps.setCapability("noReset","false");
-                //androidCaps.setCapability("appPackage","false");
-                //androidCaps.setCapability("appActivity","false");
-                //caps.setCapability("chromedriverExecutable","\"C:\\Users\\mehme\\OneDrive\\Desktop\\Appium\\chromedriver.exe\"");
-                //caps.setCapability("unlockType","pattern");
-                //caps.setCapability("unlockKey","123456789");
-                //caps.setCapability(MobileCapabilityType.APP, appUrl3);
                 String appUrl3 = System.getProperty("user.dir")
                         + File.separator + "src"
                         + File.separator + "main"
                         + File.separator + "resources"
                         + File.separator + "ApiDemos-debug.apk";
-                System.out.println("*** Android App ***");
+                System.out.println("*** Chrome Browser ***");
                 return new AndroidDriver (url,caps);
             default :
                 throw new Exception("Invalid platform");
