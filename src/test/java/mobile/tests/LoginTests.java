88a93c4b2f8cb33befc89c49f09382f23684228b
Mehmet Usta <ustamehmet83@hotmail.com>
2023-06-11 14:55:58 +0300

    AppiumPractice

--- src/test/java/mobile/tests/LoginTests.java
+++ src/test/java/mobile/tests/LoginTests.java
@@ -17,50 +17,40 @@ import java.util.HashMap;
 public class LoginTests {
     LoginPage loginPage = new LoginPage();
     ProductsPage productsPage = new ProductsPage();
-    InputStream dataIs;
-    JSONObject loginUsers;
+
 
     @BeforeClass
-    public void beforeClass() throws IOException {
-        try {
-            String dataFileName="data/loginUsers.json";
-            dataIs=getClass().getClassLoader().getResourceAsStream(dataFileName);
-            JSONTokener tokener= new JSONTokener(dataIs);
-            loginUsers= new JSONObject(tokener);
-        }catch (Exception e){
-            e.printStackTrace();
-        }finally {
-            if(dataIs != null){
-                dataIs.close();
-            }
-        }
+    public void beforeClass() {
+
 
     }
 
     @Test
-    public void invalidUsername() throws Exception {
-        BrowserUtils.enterUserName(loginUsers.getJSONObject("invalidUser").getString("username"),loginPage.usernameTextFld);
-        BrowserUtils.enterPassword(loginUsers.getJSONObject("invalidUser").getString("password"), loginPage.passTextFld);
+    public void invalidUsername() {
+        BrowserUtils.enterUserName(ConfigurationReader.loginUsers.getJSONObject("invalidUser").getString("username"), loginPage.usernameTextFld);
+        BrowserUtils.enterPassword(ConfigurationReader.loginUsers.getJSONObject("invalidUser").getString("password"), loginPage.passTextFld);
         BrowserUtils.pressLoginBtn(loginPage.loginBtn);
         String actualErrTxt = loginPage.invalidLgnTxt.getText();
-        String expectedErrTxt="Username and password do not match any user in this service.";
+        String expectedErrTxt = "Username and password do not match any user in this service.";
         Assert.assertEquals(actualErrTxt, expectedErrTxt);
 
     }
+
     @Test
-    public void invalidPassword() throws Exception {
-        BrowserUtils.enterUserName(loginUsers.getJSONObject("invalidPassword").getString("username"),loginPage.usernameTextFld);
-        BrowserUtils.enterPassword(loginUsers.getJSONObject("invalidPassword").getString("password"), loginPage.passTextFld);
+    public void invalidPassword() {
+        BrowserUtils.enterUserName(ConfigurationReader.loginUsers.getJSONObject("invalidPassword").getString("username"), loginPage.usernameTextFld);
+        BrowserUtils.enterPassword(ConfigurationReader.loginUsers.getJSONObject("invalidPassword").getString("password"), loginPage.passTextFld);
         BrowserUtils.pressLoginBtn(loginPage.loginBtn);
         String actualErrTxt = loginPage.invalidLgnTxt.getText();
-        String expectedErrTxt= ConfigurationReader.strings.get("err_valid_username_or_password");
+        String expectedErrTxt = ConfigurationReader.strings.get("err_valid_username_or_password");
         Assert.assertEquals(actualErrTxt, expectedErrTxt);
 
     }
+
     @Test
-    public void successfulLogin() throws InterruptedException {
-        BrowserUtils.enterUserName(loginUsers.getJSONObject("validUser").getString("username"),loginPage.usernameTextFld);
-        BrowserUtils.enterPassword(loginUsers.getJSONObject("validUser").getString("password"),loginPage.passTextFld);
+    public void successfulLogin() {
+        BrowserUtils.enterUserName(ConfigurationReader.loginUsers.getJSONObject("validUser").getString("username"), loginPage.usernameTextFld);
+        BrowserUtils.enterPassword(ConfigurationReader.loginUsers.getJSONObject("validUser").getString("password"), loginPage.passTextFld);
         BrowserUtils.pressLoginBtn(loginPage.loginBtn);
         productsPage.productTitle.isDisplayed();
         String productText = ConfigurationReader.strings.get("product_title");
--- src/test/java/utils/ConfigurationReader.java
+++ src/test/java/utils/ConfigurationReader.java
@@ -1,5 +1,8 @@
 package utils;
 
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -13,35 +16,34 @@ public class ConfigurationReader {
     public static HashMap<String,String> strings= new HashMap<String,String>();
     static InputStream stringsis;
     static TestUtils utils;
+    static InputStream dataIs;
+    public static JSONObject loginUsers;
 
 
   static {
       try {
           //2- We need to open file in java memory: FileInputStream
-          InputStream file=new FileInputStream("configuration.properties");
-          String xmlFileName="strings/strings.xml";
-          stringsis= ConfigurationReader.class.getClassLoader().getResourceAsStream(xmlFileName);
-
-
-          utils=new TestUtils();
-          strings=utils.parseStringXML(stringsis);
           //3- Load the properties object using FileInputStream object
+          InputStream file=new FileInputStream("configuration.properties");
           properties.load(file);
 
+          String xmlFileName="strings/strings.xml";
+          stringsis= ConfigurationReader.class.getClassLoader().getResourceAsStream(xmlFileName);
+          utils=new TestUtils();
+          strings=utils.parseStringXML(stringsis);
+
+          String dataFileName="data/loginUsers.json";
+          dataIs= ConfigurationReader.class.getClassLoader().getResourceAsStream(dataFileName);
+          JSONTokener tokener= new JSONTokener(dataIs);
+          loginUsers= new JSONObject(tokener);
 
           //close the file
           file.close();
+          stringsis.close();
+          dataIs.close();
       } catch (Exception e) {
           System.out.println("Something happened in the ConfigurationReader class.");
           e.printStackTrace();
-      }finally {
-          if (stringsis!=null){
-              try {
-                  stringsis.close();
-              } catch (IOException e) {
-                  throw new RuntimeException(e);
-              }
-          }
       }
   }
 
